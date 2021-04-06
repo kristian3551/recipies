06e86c14b9d2189f5752ee68634e875074b69ff3
Kristian Todorov <63777932+kristian3551@users.noreply.github.com>
2021-04-06 20:09:26 +0300

    Feat: added sorting functionality

--- client/src/pages/homePages/LoggedHomePage.js
+++ client/src/pages/homePages/LoggedHomePage.js
@@ -3,10 +3,11 @@ import Header from '../../components/header';
 import Footer from '../../components/footer';
 import Recipe from '../../components/recipeCard';
 import styles from './LoggedHomePage.module.css';
+import { Link } from 'react-router-dom'; 
 
 const LoggedHomePage = () => {
     const [recipes, setRecipes] = useState([]);
-    
+
     useEffect(async () => {
         const promise = await fetch('http://localhost:8000/api/recipe');
         const recipesObject = await promise.json();
@@ -14,9 +15,31 @@ const LoggedHomePage = () => {
         setRecipes(recipesObject);
     }, []);
 
+    const sortHandler = (e, type) => {
+        let sortedRecipes = [];
+        sortedRecipes = recipes.sort((a,b) => {
+            if(type === 'alphabetical') return a.meal.localeCompare(b.meal);
+            else if(type === 'likes') return b.likes.length - a.likes.length;
+            else if(type === 'comments') return b.comments.length - a.comments.length;
+            else if(type === 'ingredients') return b.ingredients.length - a.ingredients.length;
+        });
+        setRecipes(sortedRecipes);
+    }
+
     return (<>
         <Header />
         <h1 class="text-center">Our Recipes</h1>
+        <div class="dropdown">
+            <button class={`btn btn-secondary dropdown-toggle ${styles['filter-button']}`} type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Sort by
+  </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <Link class="dropdown-item" onClick={e => sortHandler(e, 'alphabetical')}>Alphabetical order</Link>
+                <Link class="dropdown-item" onClick={e => sortHandler(e, 'likes')}>By count of likes</Link>
+                <Link class="dropdown-item" onClick={e => sortHandler(e, 'comments')}>By count of comments</Link>
+                <Link class="dropdown-item" onClick={e => sortHandler(e, 'ingredients')}>By count of ingredients</Link>
+            </div>
+        </div>
 
         {[...recipes].length == 0 ? (
             <div className={styles['foodNotFound']}>
@@ -26,7 +49,7 @@ const LoggedHomePage = () => {
         ) : (
             <div id="sharedRecipes">
                 {[...recipes].map((e, i) => {
-                    return <Recipe key={i} id={e._id} recipe={e}/>
+                    return <Recipe key={i} id={e._id} recipe={e} />
                 })}
             </div>
         )}
